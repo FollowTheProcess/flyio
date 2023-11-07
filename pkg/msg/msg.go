6b4c9b434b5a3d4f27d4ad7460e069a9cbe57df1
Tom Fleet <tomfleet2018@gmail.com>
2023-11-07 20:46:06 +0000

    Implement an init message (#1)

---
+++ pkg/msg/msg.go
@@ -0,0 +1,36 @@
+// Package msg provides data structures representing the messages defined in the maelstrom message protocol.
+package msg
+
+import (
+	"encoding/json"
+
+	"github.com/valyala/fastjson"
+)
+
+// Message represents a single maelstrom message.
+type Message struct {
+	Src  string          `json:"src,omitempty"`
+	Dest string          `json:"dest,omitempty"`
+	Body json.RawMessage `json:"body,omitempty"`
+}
+
+// Type returns the type of the message.
+func (m Message) Type() string {
+	return fastjson.GetString(m.Body, "type")
+}
+
+// Body represents the common components of a maelstrom message.
+type Body struct {
+	Type      string `json:"type,omitempty"`        // The message type
+	ErrorText string `json:"text,omitempty"`        // Error message, if one occurred
+	MessageID int    `json:"msg_id,omitempty"`      // Identifier for the message, unique to the source node
+	InReplyTo int    `json:"in_reply_to,omitempty"` // Identifier for the message this message is replying to
+	ErrorCode int    `json:"code,omitempty"`        // Error code, if one occurred
+}
+
+// Init represents an init message.
+type Init struct {
+	NodeID  string   `json:"node_id,omitempty"`  // The ID of the node receiving this message
+	NodeIDs []string `json:"node_ids,omitempty"` // The IDs of all the nodes in the network (including the recipient)
+	Body
+}
