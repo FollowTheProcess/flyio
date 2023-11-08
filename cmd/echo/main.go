2dfa253622e95b2f3033ddebcc2bf82a7aa5ccba
Tom Fleet <tomfleet2018@gmail.com>
2023-11-08 19:18:49 +0000

    Add todos

--- cmd/echo/main.go
+++ cmd/echo/main.go
@@ -9,6 +9,10 @@ import (
 )
 
 func main() {
+	// TODO: I want to separate this into 3 concurrent parts:
+	// 1) A reader from stdin who's job is to decode messages and put them on a channel
+	// 2) Handlers in the middle to pull from the channel and generate replies (concurrently if possible)
+	// 3) A writer to stdout who's job is to read from the reply channel and encode it as JSON to stdout
 	node := node.New(os.Stdin, os.Stdout)
 	if err := node.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v", err)
